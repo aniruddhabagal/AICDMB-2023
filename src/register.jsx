ebfe9c43213f283678b6edaf85915bc144e7374f
Aniruddha Bagal <bagalaniruddha@gmail.com>
2022-06-24 16:28:44 +0530

    Updated Registration Link

--- src/register.jsx
+++ src/register.jsx
@@ -17,7 +17,8 @@ const Register = () => {
             <span className="reg-span">For</span>
             <li>UG / PG / PhD Scholars</li>
             <a
-              href="https://forms.gle/db4XRHH76jWcVqkEA"
+              // href="https://forms.gle/db4XRHH76jWcVqkEA"
+              href="##"
               class="text-center mb-4 mt-4 btn btn-alt mb-3 btn-border btn-border"
             >
               Register
@@ -35,7 +36,8 @@ const Register = () => {
             <span>For</span>
             <li>Academicians / Industry Persons</li>
             <a
-              href="https://forms.gle/db4XRHH76jWcVqkEA"
+              // href="https://forms.gle/db4XRHH76jWcVqkEA"
+              href="##"
               class="text-center mb-4 mt-4 btn btn-alt mb-3 btn-border btn-border"
             >
               Register
